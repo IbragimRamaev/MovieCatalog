67fb5586b24fb847e85d70a5475664b7cdad158e
Ibragim <i.zharkynbai@entwicklerburg.com>
2025-10-09 19:20:53 +0600

    initial setup program.cs

--- MovieCatalog/MovieCatalog/server/Program.cs
+++ MovieCatalog/MovieCatalog/server/Program.cs
@@ -1,25 +1,45 @@
+﻿using MovieCatalog.server.Services.Extensions;  // Imports our extension for services (we’ll create it soon)
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Registers all controllers
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Add Swagger for Api documantation and testing
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "MoviCatalog",
+        Version = "v1",
+        Description = "Backend API for MovieCatalog project"
+    });
+});
+
+//Enables CORS so React frontend can call our API
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("AllowReactApp",
+        policy => policy.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+});
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Enables Swagger only in Development mode
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+// Middleware pipeline (HTTP request handling order)
+app.UseHttpsRedirection();   // Redirects all HTTP → HTTPS
+app.UseCors("AllowReactApp"); // Enables CORS for React frontend
+app.UseAuthorization();       // Authorization middleware (we’ll use later)
+app.MapControllers();         // Maps API endpoints to controllers
+app.Run();                    // Starts the application
